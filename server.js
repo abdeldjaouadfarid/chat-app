74a506a33022ee6f805a62d9114336c604de06e0
Farid Abdeldjaouad <abdeldjaouad.farid@his.edu.dz>
2026-06-12 20:51:58 +0100

    refactor: reorganize imports and enhance server setup

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-dotenv.config()
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import http from 'http';
+import { Server } from 'socket.io';
 
-// __dirname is not available in ES modules,
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const _socketio = new Server(server);
+
+// __dirname is not available in ES modules
 // so we need to use fileURLToPath to get the current file path and then derive the directory name from it
-const metaUrl = import.meta.url
-const __filename = fileURLToPath(metaUrl)
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const PORT = process.env.PORT || 3000
-const app = express()
 
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Socket.io
+_socketio.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
